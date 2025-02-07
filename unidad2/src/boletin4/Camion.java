6b6cc6e89590d49a05ffab0ca8b00714d3cee882
AdrianChato <asolleo287@g.educaand.es>
2025-02-07 09:54:48 +0100

    Ejercicio herencias viernes 7/2

---
+++ unidad2/src/boletin4/Camion.java
@@ -0,0 +1,38 @@
+package boletin4;
+
+public class Camion extends Vehiculo {
+    private int tonelajeKilos;
+    private int numPasajeros;
+    private int numEjes;
+
+    public Camion(String dueno, int numPuertas, int numRuedas, int tonelajeKilos, int numPasajeros, int numEjes) {
+        super(dueno, numPuertas, numRuedas);
+        this.tonelajeKilos = tonelajeKilos;
+        this.numPasajeros = numPasajeros;
+        this.numEjes = numEjes;
+    }
+
+    public int getTonelajeKilos() {
+        return tonelajeKilos;
+    }
+
+    public void setTonelajeKilos(int tonelajeKilos) {
+        this.tonelajeKilos = tonelajeKilos;
+    }
+
+    public int getNumPasajeros() {
+        return numPasajeros;
+    }
+
+    public void setNumPasajeros(int numPasajeros) {
+        this.numPasajeros = numPasajeros;
+    }
+
+    public int getNumEjes() {
+        return numEjes;
+    }
+
+    public void setNumEjes(int numEjes) {
+        this.numEjes = numEjes;
+    }
+}
