a4165ecc6e20e119fb603ee128655aa8476d97b7
AdrianChato <asolleo287@g.educaand.es>
2025-02-11 12:30:56 +0100

    Martes 11/02 entorno

---
+++ unidad2/src/trabajoentorno/ejercicio1.java
@@ -0,0 +1,26 @@
+package trabajoentorno;
+
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String [] abecedario = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","v","w","x","y","Z"};
+		
+		// Funcion para calcular letra del dni 
+		//guardamos letra en lista de cadenas
+		//
+	
+	
+		}
+	
+	int letradni (Scanner input) {
+	System.out.println("Dime los números de un dni: ");
+	int dni = input.nextInt();
+	int letra = (dni)/23;
+	return letra;
+
+}
+	
+}
